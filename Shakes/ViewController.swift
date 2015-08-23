f9b2ac49429e9201b5d6a619e91c90a800f2d567
Takuto Sato <takuto@Tacknyan.local>
2015-08-23 16:58:40 +0900

    Initial Commit

---
+++ Shakes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shakes
+//
+//  Created by Takuto Sato on 2015/08/23.
+//  Copyright (c) 2015年 Fringe81 co, Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
